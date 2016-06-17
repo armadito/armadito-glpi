cf8e1fd1a5f11854e568c5afd4e40610621ad51e
Valentin Hamon <vhamon@teclib.com>
2016-06-17 12:09:47 +0200

    Add first json_parsing implementation poc

--- index.php
+++ index.php
@@ -2,11 +2,25 @@
 
 // Page qui reçoit les requêtes de l'agent
 
-include_once ("../../../inc/includes.php");
+// include_once ("../../inc/includes.php");
 include_once ("inc/armadito-includes.php");
 
 if($_SERVER['REQUEST_METHOD'] == "POST"){ 
-	echo "Hello armadito !";
+
+   if(isset($_POST['content-json'])){
+
+      $json = json_decode($_POST['content-json']);
+
+      if(json_last_error() == JSON_ERROR_NONE)
+      {
+         echo "json::success";
+         var_dump($json);
+      }
+      else
+      {
+         echo "json::".json_last_error_msg();
+      }
+   }
 }
 
 ?>
