d30c49ea57ce485afca56351440af2e6f511c170
Valentin Hamon <vhamon@teclib.com>
2016-07-13 14:38:38 +0200

    Improve enrollment of agents in database

--- inc/enrollment.class.php
+++ inc/enrollment.class.php
@@ -77,10 +77,115 @@ class PluginArmaditoEnrollment {
     **/
      function enroll(){
 
-         $error = $this->enrollNewDevice();
+         if($this->isAlreadyEnrolled()) {
+            $error = $this->updateEnrolledDevice();
+         }
+         else {
+            $error = $this->enrollNewDevice();
+         }
 
          return $error;
      }
+
+    /**
+    * Check if device is already enrolled
+    *
+    * @return TRUE or FALSE
+    **/
+    function isAlreadyEnrolled(){
+      global $DB;
+
+      PluginArmaditoToolbox::validateInt($this->jobj->agent_id);
+      
+      PluginArmaditoToolbox::logE("Agent_id = ".$this->jobj->agent_id);
+
+      $query = "SELECT last_contact FROM `glpi_plugin_armadito_armaditos`
+                 WHERE `id`='".$this->jobj->agent_id."'";
+      $ret = $DB->query($query); 
+       
+      if(!$ret){
+         PluginArmaditoToolbox::logE("Error isAlreadyEnrolled : ".$DB->error());
+         return false;
+      }
+           
+      if($DB->numrows($ret) > 0){
+         return true;
+      } 
+      
+      return false;
+    }
+
+    /**
+    * Try to enroll a new Armadito device
+    *
+    * @return PluginArmaditoError obj
+    **/
+     function updateEnrolledDevice(){
+         global $DB;
+
+         $this->setAgentid($this->jobj->agent_id);
+
+         $error = new PluginArmaditoError();
+         
+         $query = "UPDATE `glpi_plugin_armadito_armaditos`
+                 SET `entities_id`=?, 
+                     `computers_id`=?,
+                     `plugin_fusioninventory_agents_id`=?,
+                     `device_id`=?,
+                     `agent_version`=?,
+                     `antivirus_name`=?,
+                     `antivirus_version`=?,
+                     `antivirus_state`=?,
+                     `last_contact`=?,
+		               `last_alert`=?
+                  WHERE `agent_id`=?";
+
+         $stmt = $DB->prepare($query);
+
+         if(!$stmt) {
+            $error->setMessage(1, 'Enrollment insert preparation failed.');
+            $error->log();
+            return $error;
+         }
+
+         if(!$stmt->bind_param('iiisssssssi', $entities_id, $computers_id, $fusion_table_id, $fusion_device_id, $agent_version, $antivirus_name, $antivirus_version, $antivirus_state, $last_contact, $last_alert, $agent_id)) {
+            $error->setMessage(1, 'Enrollment insert bin_param failed (' . $stmt->errno . ') ' . $stmt->error);
+            $error->log();
+            $stmt->close();
+            return $error;
+          }
+         
+         # We set values
+         $entities_id = 0;
+         $computers_id = 0;
+         $fusion_table_id = 0;
+         $fusion_device_id = $this->jobj->fusion_id;
+         $agent_version = $this->jobj->agent_version;
+         $antivirus_name = $this->jobj->task->antivirus->name;
+         $antivirus_version = $this->jobj->task->antivirus->version;
+         $antivitus_state = "unknown";
+         $last_contact = date("Y-m-d H:i:s", time());
+         $last_alert = '1970-01-01 00:00:00';
+         $agent_id = $this->jobj->agent_id;
+
+         if(!$stmt->execute()){
+            $error->setMessage(1, 'Enrollment insert execution failed (' . $stmt->errno . ') ' . $stmt->error);
+            $error->log();
+            $stmt->close();
+            return $error;
+         }
+
+         $stmt->close();
+
+         PluginArmaditoToolbox::logIfExtradebug(
+            'pluginArmadito-Enrollment',
+            'ReEnroll Device with id '.$this->agentid
+         );
+
+         $error->setMessage(1, 'New device successfully re-enrolled.');
+
+         return $error;
+    }
 
     /**
     * Try to enroll a new Armadito device
@@ -96,38 +201,38 @@ class PluginArmaditoEnrollment {
 
          $stmt = $DB->prepare($query);
 
-         if($stmt) {
-
-            if(!$stmt->bind_param('iiisssssss', $entities_id, $computers_id, $fusion_table_id, $fusion_device_id, $agent_version, $antivirus_name, $antivirus_version, $antivirus_state, $last_contact, $last_alert)) {
-               $error->setMessage(1, 'Enrollment insert bin_param failed (' . $stmt->errno . ') ' . $stmt->error);
-               $error->log();
-               $stmt->close();
-            }
-
-            # We set values
-            $entities_id = 0;
-            $computers_id = 0;
-            $fusion_table_id = 0;
-            $fusion_device_id = $this->jobj->fusion_id;
-            $agent_version = $this->jobj->agent_version;
-            $antivirus_name = $this->jobj->task->antivirus->name;
-            $antivirus_version = $this->jobj->task->antivirus->version;
-            $antivitus_state = "unknown";
-            $last_contact = date("Y-m-d H:i:s", time());
-            $last_alert = '1970-01-01 00:00:00';
-
-            if(!$stmt->execute()){
-               $error->setMessage(1, 'Enrollment insert execution failed (' . $stmt->errno . ') ' . $stmt->error);
-               $error->log();
-               $stmt->close();
-               return $error;
-            }
-         }
-         else {
+         if(!$stmt) {
             $error->setMessage(1, 'Enrollment insert preparation failed.');
             $error->log();
             return $error;
          }
+
+         if(!$stmt->bind_param('iiisssssss', $entities_id, $computers_id, $fusion_table_id, $fusion_device_id, $agent_version, $antivirus_name, $antivirus_version, $antivirus_state, $last_contact, $last_alert)) {
+               $error->setMessage(1, 'Enrollment insert bin_param failed (' . $stmt->errno . ') ' . $stmt->error);
+               $error->log();
+               $stmt->close();
+               return $error;
+         }
+
+         # We set values
+         $entities_id = 0;
+         $computers_id = 0;
+         $fusion_table_id = 0;
+         $fusion_device_id = $this->jobj->fusion_id;
+         $agent_version = $this->jobj->agent_version;
+         $antivirus_name = $this->jobj->task->antivirus->name;
+         $antivirus_version = $this->jobj->task->antivirus->version;
+         $antivitus_state = "unknown";
+         $last_contact = date("Y-m-d H:i:s", time());
+         $last_alert = '1970-01-01 00:00:00';
+
+         if(!$stmt->execute()){
+            $error->setMessage(1, 'Enrollment insert execution failed (' . $stmt->errno . ') ' . $stmt->error);
+            $error->log();
+            $stmt->close();
+            return $error;
+         }
+         
 
          $stmt->close();
 
