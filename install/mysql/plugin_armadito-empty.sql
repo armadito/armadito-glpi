f1adf59a554cd6bb6d4e5fe81a26daa57a636842
Valentin Hamon <vhamon@teclib.com>
2016-07-18 13:48:12 +0200

    Add alerts table in database sql

--- install/mysql/plugin_armadito-empty.sql
+++ install/mysql/plugin_armadito-empty.sql
@@ -37,4 +37,13 @@ CREATE TABLE `glpi_plugin_armadito_states` (
    PRIMARY KEY (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1;
 
+DROP TABLE IF EXISTS `glpi_plugin_armadito_alerts`;
+
+CREATE TABLE `glpi_plugin_armadito_alerts` (
+   `id` int(11) NOT NULL AUTO_INCREMENT,
+   `type` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+   `value` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+   PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1;
+
 
