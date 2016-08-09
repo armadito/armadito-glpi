313c2dfb5d8e49616a5329b28a441d8c401425e0
Valentin Hamon <vhamon@teclib.com>
2016-08-09 16:03:30 +0200

    Add chart classes

---
+++ inc/chart.class.php
@@ -0,0 +1,50 @@
+<?php
+/**
+
+   Copyright (C) 2010-2016 by the FusionInventory Development Team.
+   Copyright (C) 2016 Teclib'
+
+   This file is part of Armadito Plugin for GLPI.
+
+   Armadito Plugin for GLPI is free software: you can redistribute it and/or modify
+   it under the terms of the GNU Affero General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   Armadito Plugin for GLPI is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   GNU Affero General Public License for more details.
+
+   You should have received a copy of the GNU Affero General Public License
+   along with Armadito Plugin for GLPI. If not, see <http://www.gnu.org/licenses/>.
+
+**/
+
+if (!defined('GLPI_ROOT')) {
+   die("Sorry. You can't access directly to this file");
+}
+
+class PluginArmaditoChart extends CommonGLPI {
+
+   protected $title;
+   protected $data;
+   protected $name;
+   protected $width;
+
+   function __construct() {
+      //
+   }
+
+   function init($name_, $title_, $data_, $width_=400) {
+      $this->name = $name_;
+      $this->title = $title_;
+      $this->data = $data_;
+      $this->width = $width_;
+   }
+
+   function showBackground (){
+      echo '<svg width="'.$this->width.'" style="background-color: #f3f3f3;" id="'.$this->name.'"></svg>';
+   }
+}
+?>
