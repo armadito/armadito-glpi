1a438ca65f05b419b8c634bf75b5ad260be8de82
Valentin Hamon <vhamon@teclib.com>
2016-09-20 15:11:25 +0200

    Remove deprecated debug in phpunit script

--- phpunit/tools/format_phpunitlogs.sh
+++ phpunit/tools/format_phpunitlogs.sh
@@ -3,21 +3,13 @@ scriptdir=$(dirname ${0})
 
 rm ${1}/build/logs/log.xml ${1}/build/logs/log.html
 
-set -e
-
-echo "PRE xsltproc"
-
 echo "<testsuites />" | xsltproc \
     --output ${1}/build/logs/log.xml \
     --stringparam basedir ${1} \
     ${scriptdir}/merge.xslt -
-
-echo "POST xsltproc merge"
 
 xsltproc \
     --stringparam projectname ${2} \
     --output ${1}/build/logs/log.html \
     ${scriptdir}/phpunit.log.xslt \
     ${1}/build/logs/log.xml
-
-echo "POST xsltproc phpunit"
