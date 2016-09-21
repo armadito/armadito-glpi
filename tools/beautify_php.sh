927e32fdb1a36bb4bbe97b82aa3ddd6b08b04d68
Valentin Hamon <vhamon@teclib.com>
2016-09-21 15:55:16 +0200

    update beautifier

--- tools/beautify_php.sh
+++ tools/beautify_php.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # NewLines(before=if:switch:while:for:foreach:function:T_CLASS:return:break,after=T_COMMENT:return)
 
-php_beautifier --filters "Pear()" -r "../*.php" "./armadito-beautified/" 
+php_beautifier --filters "NewLines(before=if:switch:while:for:foreach:function:T_CLASS:return:break,after=T_COMMENT:return)" -r "../*.php" "./armadito-beautified/" 
 cp -rf ./armadito-beautified/* ../
 rm -r ./armadito-beautified
