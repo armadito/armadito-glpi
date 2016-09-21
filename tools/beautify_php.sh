830ec11399a7ec14fa85ca8d727a1b31842496a0
Valentin Hamon <vhamon@teclib.com>
2016-09-21 15:32:03 +0200

    change beautify options

--- tools/beautify_php.sh
+++ tools/beautify_php.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+# NewLines(before=if:switch:while:for:foreach:function:T_CLASS:return:break,after=T_COMMENT:return)
 
-php_beautifier -r "../*.php" "./armadito-beautified/"
+php_beautifier --filters "Pear()" -r "../*.php" "./armadito-beautified/" 
 cp -rf ./armadito-beautified/* ../
 rm -r ./armadito-beautified
