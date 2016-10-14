d2429fa3cac81cb1f2cea74a5c29b5c21dba11f0
Valentin Hamon <vhamon@teclib.com>
2016-10-14 10:05:54 +0200

    Fix trailing whitespaces

--- tools/travis_build.sh
+++ tools/travis_build.sh
@@ -3,6 +3,6 @@
 set -e
 
 if [[ $TRAVIS_BRANCH == 'coverity_scan' ]]
-  cp -r /home/travis/build/armadito/glpi/plugins/_lib /home/travis/build/armadito/glpi/plugins/armadito/lib 
+  cp -r /home/travis/build/armadito/glpi/plugins/_lib /home/travis/build/armadito/glpi/plugins/armadito/lib
 
 ant -Dclearsavepoint='true' -Dbasedir=. -f ./glpi/plugins/armadito/phpunit/build.xml phpunit.all
