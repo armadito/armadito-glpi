5f1596a125345cae7723996858e8c526f0891dcf
Valentin Hamon <vhamon@teclib.com>
2016-10-14 11:02:55 +0200

    Does not move into directory from inside script for coverity_scan

--- tools/prepend_coverity.sh
+++ tools/prepend_coverity.sh
@@ -4,5 +4,3 @@ set -e
 
 mv /home/travis/build/armadito/glpi/plugins/armadito/lib /home/travis/build/armadito/armadito_lib
 mv /home/travis/build/armadito/glpi/lib /home/travis/build/armadito/glpi_lib
-
-cd /home/travis/build/armadito/glpi/plugins/armadito
