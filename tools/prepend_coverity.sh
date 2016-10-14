e5523f7358db11f6e12ee2b65c48c400e2645eb4
Valentin Hamon <vhamon@teclib.com>
2016-10-14 10:36:06 +0200

    Set up a way to avoid scanning js lib files during coverity scan

---
+++ tools/prepend_coverity.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -e
+
+mv /home/travis/build/armadito/glpi/plugins/armadito/lib /home/travis/build/armadito/armadito_lib
+mv /home/travis/build/armadito/glpi/lib /home/travis/build/armadito/glpi_lib
+
+cd /home/travis/build/armadito/glpi/plugins/armadito
--- tools/travis_build.sh
+++ tools/travis_build.sh
@@ -4,8 +4,8 @@ set -e
 
 if [[ $TRAVIS_BRANCH == 'coverity_scan' ]]
 then
-	cp -r /home/travis/build/armadito/glpi/plugins/_lib /home/travis/build/armadito/glpi/plugins/armadito/lib
+	cp -r /home/travis/build/armadito/armadito_lib /home/travis/build/armadito/glpi/plugins/armadito/lib
+	cp -r /home/travis/build/armadito/glpi_lib /home/travis/build/armadito/glpi/lib
 fi
 
 ant -Dclearsavepoint='true' -Dbasedir=. -f ./glpi/plugins/armadito/phpunit/build.xml phpunit.all
-
