2f62da5eafa8613687101408d1081dc72268f4fd
Valentin Hamon <vhamon@teclib.com>
2016-10-14 13:08:53 +0200

    Coverity scan only plugin

--- tools/travis_build.sh
+++ tools/travis_build.sh
@@ -5,7 +5,7 @@ set -e
 if [[ $TRAVIS_BRANCH == 'coverity_scan' ]]
 then
 	cp -r /home/travis/build/armadito/armadito_lib /home/travis/build/armadito/glpi/plugins/armadito/lib
-	cp -r /home/travis/build/armadito/glpi_lib /home/travis/build/armadito/glpi/lib
+#	cp -r /home/travis/build/armadito/glpi_lib /home/travis/build/armadito/glpi/lib
 fi
 
 ant -Dclearsavepoint='true' -Dbasedir=. -f ./glpi/plugins/armadito/phpunit/build.xml phpunit.all
