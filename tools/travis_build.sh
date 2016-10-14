ff9fc94921915acb05445349b440e2b1f25565a7
Valentin Hamon <vhamon@teclib.com>
2016-10-14 10:22:11 +0200

    Fix conditional in travis_build script

--- tools/travis_build.sh
+++ tools/travis_build.sh
@@ -3,7 +3,9 @@
 set -e
 
 if [[ $TRAVIS_BRANCH == 'coverity_scan' ]]
-  cp -r /home/travis/build/armadito/glpi/plugins/_lib /home/travis/build/armadito/glpi/plugins/armadito/lib
+then
+	cp -r /home/travis/build/armadito/glpi/plugins/_lib /home/travis/build/armadito/glpi/plugins/armadito/lib
+fi
 
 ant -Dclearsavepoint='true' -Dbasedir=. -f ./glpi/plugins/armadito/phpunit/build.xml phpunit.all
 
