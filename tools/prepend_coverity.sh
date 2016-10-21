97bf94c575102a7b96397cf84228af5b3c40cced
Valentin Hamon <vhamon@teclib.com>
2016-10-21 14:36:14 +0200

    Does not move glpi_lib dir anymore

--- tools/prepend_coverity.sh
+++ tools/prepend_coverity.sh
@@ -3,4 +3,4 @@
 set -e
 
 mv /home/travis/build/armadito/glpi/plugins/armadito/lib /home/travis/build/armadito/armadito_lib
-mv /home/travis/build/armadito/glpi/lib /home/travis/build/armadito/glpi_lib
+#mv /home/travis/build/armadito/glpi/lib /home/travis/build/armadito/glpi_lib
