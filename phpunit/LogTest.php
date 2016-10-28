1b61f91564a1bad67746f42f7f03894c8d532e54
Valentin Hamon <vhamon@teclib.com>
2016-10-28 11:18:27 +0200

    Rm unused class

--- phpunit/LogTest.php
+++ phpunit/LogTest.php
@@ -23,42 +23,18 @@ along with Armadito Plugin for GLPI. If not, see <http://www.gnu.org/licenses/>.
 
 class GLPIlogs extends PHPUnit_Framework_TestCase
 {
-
     public function testSQLlogs()
     {
-
         $filecontent = file_get_contents(GLPI_ROOT . "/files/_log/sql-errors.log");
-
         $this->assertEquals('', $filecontent, 'sql-errors.log not empty');
-
         file_put_contents(GLPI_ROOT . "/files/_log/sql-errors.log", '');
     }
 
-
-
     public function testPHPlogs()
     {
-
         $filecontent = file_get_contents(GLPI_ROOT . "/files/_log/php-errors.log");
-
         $this->assertEquals('', $filecontent, 'php-errors.log not empty');
-
         file_put_contents(GLPI_ROOT . "/files/_log/php-errors.log", '');
     }
-
 }
-
-
-
-class GLPIlogs_AllTests
-{
-
-    public static function suite()
-    {
-
-        $suite = new PHPUnit_Framework_TestSuite('GLPIlogs');
-        return $suite;
-    }
-}
-
 ?>
