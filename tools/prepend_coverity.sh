2ffb58689b37c1dce2437a282aef5be0a9c04ff2
Valentin Hamon <vhamon@teclib.com>
2016-10-28 14:25:39 +0200

    Fix travis stuff, rm post-script commands

--- tools/prepend_coverity.sh
+++ tools/prepend_coverity.sh
@@ -3,4 +3,3 @@
 set -e
 
 mv /home/travis/build/armadito/glpi/plugins/armadito/lib /home/travis/build/armadito/armadito_lib
-#mv /home/travis/build/armadito/glpi/lib /home/travis/build/armadito/glpi_lib
--- tools/travis_build.sh
+++ tools/travis_build.sh
@@ -5,7 +5,6 @@ set -e
 if [[ $TRAVIS_BRANCH == 'coverity_scan' ]]
 then
 	cp -r /home/travis/build/armadito/armadito_lib /home/travis/build/armadito/glpi/plugins/armadito/lib
-	# cp -r /home/travis/build/armadito/glpi_lib /home/travis/build/armadito/glpi/lib
 fi
 
-ant -Dclearsavepoint='true' -Dbasedir=. -f ./glpi/plugins/armadito/phpunit/build.xml phpunit.all
+ant -Dclearsavepoint='true' -Dbasedir=. -f ./phpunit/build.xml phpunit.all
