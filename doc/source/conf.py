b14e457ad1ed11c0056281127903d14d44b5b852
Valentin Hamon <vhamon@teclib.com>
2016-11-07 12:14:54 +0100

    Fix bad conf adn copyright

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -48,9 +48,9 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'Armadito'
-copyright = u'2016, François Déchelle, Valentin Hamon, Ulrich Fausther'
-author = u'François Déchelle, Valentin Hamon, Ulrich Fausther'
+project = u'Armadito for GLPI'
+copyright = u'2016, Teclib'
+author = u'Teclib'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -236,8 +236,8 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'Armadito.tex', u'Armadito Documentation',
-     u'François Déchelle, Valentin Hamon, Ulrich Fausther', 'manual'),
+    (master_doc, 'Armadito.tex', u'Armadito for GLPI Documentation',
+     u'Teclib', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -266,7 +266,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'armadito', u'Armadito Documentation',
+    (master_doc, 'armadito', u'Armadito for GLPI Documentation',
      [author], 1)
 ]
 
@@ -280,7 +280,7 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'Armadito', u'Armadito Documentation',
+    (master_doc, 'Armadito', u'Armadito for GLPI Documentation',
      author, 'Armadito', 'One line description of project.',
      'Miscellaneous'),
 ]
