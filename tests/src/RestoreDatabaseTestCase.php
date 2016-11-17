24794e8433e7504ad0fc2511b94ecaa4b52af838
Valentin Hamon <vhamon@teclib.com>
2016-11-17 10:40:30 +0100

    Add missing tests/src dir

---
+++ tests/src/RestoreDatabaseTestCase.php
@@ -0,0 +1,22 @@
+<?php
+include_once('./conf/bootstrap.php');
+include_once('commonfunction.php');
+
+include_once(GLPI_ROOT . "/config/based_config.php");
+include_once(GLPI_ROOT . "/inc/dbmysql.class.php");
+include_once(GLPI_CONFIG_DIR . "/config_db.php");
+
+
+/*
+ * Helper class to restore database from some SQL restore point file
+ */
+
+abstract class RestoreDatabaseTestCase extends CommonTestCase
+{
+
+    public static function setUpBeforeClass()
+    {
+        self::restore_database();
+    }
+}
+?>
