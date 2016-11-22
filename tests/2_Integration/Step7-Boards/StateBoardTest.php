6bd39812c94a3a8a60bae95a048f86fc98b78fe4
Valentin Hamon <vhamon@teclib.com>
2016-11-22 09:47:40 +0100

    Add boards integration tests

---
+++ tests/2_Integration/Step7-Boards/StateBoardTest.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+Copyright (C) 2016 Teclib'
+
+This file is part of Armadito Plugin for GLPI.
+
+Armadito Plugin for GLPI is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Armadito Plugin for GLPI is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with Armadito Plugin for GLPI. If not, see <http://www.gnu.org/licenses/>.
+
+**/
+
+class StateBoardTest extends CommonTestCase
+{
+    /**
+     * @test
+     */
+    public function display()
+    {
+        ob_start();
+        $_SESSION['glpishowallentities'] = true;
+        $_SESSION['glpiparententities']  = '';
+        $_SESSION['glpiactiveentities_string'] = '0';
+
+        $board = new PluginArmaditoStateBoard();
+        $board->displayBoard();
+        ob_end_clean();
+    }
+}
+
