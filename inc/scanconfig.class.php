b4a451c0374bfd2a652f1ab387879795184a73da
Valentin Hamon <vhamon@teclib.com>
2016-11-24 12:03:21 +0100

    Encode in base64 scan_options to deploy

--- inc/scanconfig.class.php
+++ inc/scanconfig.class.php
@@ -75,7 +75,7 @@ class PluginArmaditoScanConfig extends PluginArmaditoCommonDBTM
                   "scanconfig_id": ' . $this->id . ',
                   "scan_name": "' . $this->scan_name . '",
                   "scan_path": "' . $this->scan_path . '",
-                  "scan_options": "' . $this->scan_options . '"
+                  "scan_options": "' . base64_encode($this->scan_options) . '"
                }';
     }
 
