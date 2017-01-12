eb8c65bfa5e8bf727f9cc94a9a61e334930fdcbd
Valentin Hamon <vhamon@teclib.com>
2017-01-12 15:11:42 +0100

    Close #163 - Add enrollment key tests

---
+++ tests/2_Integration/01-Init/0-EnrollmentKeyTest.php
@@ -0,0 +1,56 @@
+<?php
+
+/*
+Copyright (C) 2016 Teclib'
+
+This file is part of Armadito Plugin for GLPI.
+
+Armadito Plugin for GLPI is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Armadito Plugin for GLPI is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with Armadito Plugin for GLPI. If not, see <http://www.gnu.org/licenses/>.
+
+**/
+
+class EnrollmentKeyTest extends CommonTestCase
+{
+    /**
+     * @test
+     */
+    public function FormAdd()
+    {
+        $this->NewEnrollmentKey(1, "2017-01-12 11:06","2017-01-12 13:06", 5, "0RV2V-2RYTX-NX4OW-7LARZ-KZQAJ");
+        $this->NewEnrollmentKey(2, "2017-01-12 11:06","2019-01-12 11:16", 15, "9LTKF-WTDYW-MFU83-XKYEN-P4VEO");
+    }
+
+    protected function NewEnrollmentKey($id, $creation_date, $expiration_date , $use_counter, $key)
+    {
+        $this->initEnrollmentKey($id, $creation_date, $expiration_date , $use_counter);
+        $this->value = $key;
+
+        $enrollmentkey->insertEnrollmentKeyInDB();
+    }
+
+    protected function initEnrollmentKey($id, $creation_date, $expiration_date, $use_counter)
+    {
+        $data = array(
+            "id" => $id,
+            "creation_date" => $creation_date,
+            "expiration_date" => $expiration_date,
+            "use_counter" => $use_counter
+        );
+
+        $enrollmentkey = new PluginArmaditoEnrollmentKey();
+        $enrollmentkey->initFromForm($data);
+        return $enrollmentkey;
+    }
+}
+
--- tests/2_Integration/01-Init/1-ApiAgentsTest.php
+++ tests/2_Integration/01-Init/1-ApiAgentsTest.php
@@ -29,21 +29,21 @@ class ApiAgentsTest extends CommonTestCase
     {
         // Scheduler id should be from 0 to N without any gaps
         // Agent id is just an auto_incremented id
-        $this->insertOrUpdateAgent(0, 1, 1, "4C4C4544-0033-4A10-8051-BBBBBBBBBBBB", "0RV2V-2RYTX-NX4OW-7LARZ-KZQAJ");
-        $this->insertOrUpdateAgent(0, 2, 2, "4C4C4544-0033-4A10-8051-FFFFFFFFFFFF", "0RV2V-2RYTX-NX4OW-7LARZ-KZQAJ");
-        $this->insertOrUpdateAgent(2, 2, 2, "4C4C4544-0033-4A10-8051-FFFFFFFFFFFF", "0RV2V-2RYTX-NX4OW-7LARZ-KZQAJ");
-        $this->insertOrUpdateAgent(0, 2, 2, "4C4C4544-0033-4A10-8051-FFFFFFFFFFFF", "0RV2V-2RYTX-NX4OW-7LARZ-KZQAJ");
+        $this->insertOrUpdateAgent(0, 1, 1, "4C4C4544-0033-4A10-8051-BBBBBBBBBBBB");
+        $this->insertOrUpdateAgent(0, 2, 2, "4C4C4544-0033-4A10-8051-FFFFFFFFFFFF");
+        $this->insertOrUpdateAgent(2, 2, 2, "4C4C4544-0033-4A10-8051-FFFFFFFFFFFF");
+        $this->insertOrUpdateAgent(0, 2, 2, "4C4C4544-0033-4A10-8051-FFFFFFFFFFFF");
 
         $this->purgeAgent(1);
 
-        $this->insertOrUpdateAgent(0, 3, 1, "4C4C4544-0033-4A10-8051-BBBBBBBBBBBB", 1, "0RV2V-2RYTX-NX4OW-7LARZ-KZQAJ");
-        $this->insertOrUpdateAgent(0, 4, 3, "4C4C4544-0033-4A10-8051-AAAAAAAAAAAA", 1, "0RV2V-2RYTX-NX4OW-7LARZ-KZQAJ");
+        $this->insertOrUpdateAgent(0, 3, 1, "4C4C4544-0033-4A10-8051-BBBBBBBBBBBB", 1);
+        $this->insertOrUpdateAgent(0, 4, 3, "4C4C4544-0033-4A10-8051-AAAAAAAAAAAA", 1);
     }
 
-    protected function insertOrUpdateAgent($current_agentid, $new_agentid, $new_schedulerid, $uuid, $enrollmentkey)
+    protected function insertOrUpdateAgent($current_agentid, $new_agentid, $new_schedulerid, $uuid)
     {
         $json  = '{
-  "task": {"obj":"{ "key": "'.$enrollmentkey.'" }",
+  "task": {"obj":"{}",
            "name":"Enrollment",
            "antivirus": {
                          "name":"Armadito",
