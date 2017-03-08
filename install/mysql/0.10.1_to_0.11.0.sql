169d3e0e1ce1f0e84b0f0dc693818bdb2a78fca8
Valentin Hamon <vhamon@teclib.com>
2017-03-08 15:14:31 +0100

    Update sql files for schedulerdetails

--- install/mysql/0.10.1_to_0.11.0.sql
+++ install/mysql/0.10.1_to_0.11.0.sql
@@ -9,3 +9,6 @@ CREATE TABLE `glpi_plugin_armadito_schedulerdetails` (
    PRIMARY KEY (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1;
 
+ALTER TABLE `glpi_plugin_armadito_schedulers` ADD `plugin_armadito_antiviruses_id` int(11) NOT NULL;
+ALTER TABLE `glpi_plugin_armadito_schedulers` ADD `plugin_armadito_schedulerdetails_id` int(11) NOT NULL;
+
