a3cf3f558478d8b83ce5aaffd95c680f36e4f9c2
Valentin Hamon <vhamon@teclib.com>
2017-03-08 16:17:32 +0100

    unactivate Board section of scheduler menu

--- inc/menu.class.php
+++ inc/menu.class.php
@@ -236,7 +236,7 @@ class PluginArmaditoMenu extends CommonGLPI
 
         if (Session::haveRight('plugin_armadito_schedulers', READ))
         {
-            $menu_entries[] = new PluginArmaditoMenuEntry('Board', self::BOARD_PNG, 'schedulerboard.php');
+           // $menu_entries[] = new PluginArmaditoMenuEntry('Board', self::BOARD_PNG, 'schedulerboard.php');
             $menu_entries[] = new PluginArmaditoMenuEntry('Schedulers', self::LISTING_PNG, 'scheduler.php');
         }
 
